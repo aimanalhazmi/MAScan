c843c71646be84823ae6c80288edeee4969e7158
txmbau <baumann.tim@protonmail.com>
2026-05-21 18:53:37 +0200

    feat(tools): implement firecrawl search and query

--- src/mascan/tools/web_query.py
+++ src/mascan/tools/web_query.py
@@ -5,18 +5,43 @@ Inspired by Langchain's WebSearchTool.
 """
 
 import os
+import requests
 
 from langchain.tools import tool
 from dotenv import load_dotenv
+from firecrawl import Firecrawl
 
 load_dotenv()
 
 FIRECRAWL_API_KEY = os.getenv("FIRECRAWL_API_KEY")
+FIRECRAWL_QUERY_LIMIT = 3
+
+firecrawl_client = Firecrawl(api_key=FIRECRAWL_API_KEY)
 
 @tool
-def web_query():
-    """Only use this tool to perform web queries. Do not use it for any other purpose."""
-    print("---Web Query Tool called---")
+def web_query(
+    query: str,
+) -> str:
+    """
+    Perform a web query using the FireCrawl API. Only use this tool for web queries.
+    Args:
+        query (str): The search query to perform.
+    Returns:
+        str: The search results from the FireCrawl API.
+    """
+
+    results = firecrawl_client.search(
+        query,
+        limit=FIRECRAWL_QUERY_LIMIT,
+        scrape_options={
+            "formats": ["markdown", "links"],
+            "blockAds": True
+        }
+        )
+    
+    return results
 
 if __name__ == "__main__":
-    web_query.invoke({})
+
+    results = web_query.invoke({"query": "What is the capital of France?"})
+    print(results)
