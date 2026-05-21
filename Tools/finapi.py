3598f8d413495560d47abec06e822d8fea57d4b2
txmbau <baumann.tim@protonmail.com>
2026-05-21 15:35:03 +0200

    fix: add docstring to dummy tools

--- Tools/finapi.py
+++ Tools/finapi.py
@@ -2,4 +2,8 @@ from langchain.tools import tool
 
 @tool
 def finapi():
+    """Only use this tool to perform financial API queries. Do not use it for any other purpose."""
     print("---FinAPI Tool called---")
+
+if __name__ == "__main__":
+    finapi.invoke({})
--- Tools/webquery.py
+++ Tools/webquery.py
@@ -2,4 +2,8 @@ from langchain.tools import tool
 
 @tool
 def web_query():
+    """Only use this tool to perform web queries. Do not use it for any other purpose."""
     print("---Web Query Tool called---")
+
+if __name__ == "__main__":
+    web_query.invoke({})
